e5cc809417754f49af29716924a7601ae365737b
Charles Zilm <ch@rleszilm.com>
2017-08-16 21:00:03 -0700

    Second pass
    Make it so non-vars can be passed to istype. Optimize non-vars so
    that expressions are only evaulated once. Add unit tests.

--- src/istype.erl
+++ src/istype.erl
@@ -1,13 +1,63 @@
 -module(istype).
 
 %% API exports
--export([]).
+-export([return_atom/0]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-dialyzer({nowarn_function, [invocation_test/0,
+                             complex_types_test/0,
+                             range_test/0,
+                             assert_test/0]}).
+
+-type complex_type()          :: binary() | integer() | atom.
+-type compound_complex_type() :: complex_type() | float().
+-type int_range_type()        :: 1..100.
+-type char_range_type()       :: 0..16#ff.
+%%====================================================================
+%% Test functions
+%%====================================================================
+invocation_test() ->
+	TestAtom = atom,
+	true = istype(atom, atom()),
+	true = istype(TestAtom, atom()),
+	true = istype(fun() -> atom end(), atom()),
+	true = istype(return_atom(), atom()),
+	true = istype(?MODULE:return_atom(), atom()),
+	true = istype(case atom of X0 -> X0 end, atom()),
+	true = istype(begin return_atom() end, atom()),
+	true = case ?MODULE:return_atom() of
+	           X1 when istype(X1, atom()) -> true
+	       end.
+
+complex_types_test() ->
+	true  = istype(<<"hello">>, complex_type()),
+	true  = istype(1,           complex_type()),
+	true  = istype(atom,        complex_type()),
+	false = istype([],          complex_type()),
+	true  = istype(7.0,         compound_complex_type()).
+
+range_test() ->
+	true  = istype(1,      int_range_type()),
+	true  = istype(50,     int_range_type()),
+	true  = istype(100,    int_range_type()),
+	false = istype(0,      int_range_type()),
+	false = istype(101,    int_range_type()),
+	true  = istype($a,     char_range_type()),
+	false = istype(16#fff, char_range_type()).
+
+assert_test() ->
+	asserttype(atom, atom()),
+	true = try
+	           asserttype(<<"binary">>, atom()),
+	           false
+	       catch
+	           error:{badmatch, false} ->
+	               true
+	       end.
 
 %%====================================================================
-%% API functions
+%% Utility functions
 %%====================================================================
-
-
-%%====================================================================
-%% Internal functions
-%%====================================================================
+return_atom() ->
+	atom.
--- src/istype_transform.erl
+++ src/istype_transform.erl
@@ -1,6 +1,8 @@
 -module(istype_transform).
 -export([parse_transform/2]).
 
+%% @doc
+%% @end
 parse_transform(Forms, _Options) ->
 	UserTypes = forms:reduce(fun get_types/2, #{}, Forms),
 	forms:map(fun(Form) -> do_transform(Form, UserTypes) end, Forms).
@@ -9,63 +11,88 @@ get_types({attribute, _, type, {Type, TypeInfo, []}}, Acc) ->
 	Acc#{Type => TypeInfo};
 get_types(_, Acc) -> Acc.
 
-do_transform({call, Line, {atom, _, istype}, [{var, _, Var}, {call, _, {atom, _, Type}, TypeArgs}]}, UserTypes) ->
-	istype_to_gaurd(Line, Var, {type, Line, Type, TypeArgs}, UserTypes);
+do_transform({call, Line, {atom, _, istype}, [Value, {call, _, {atom, _, Type}, TypeArgs}]}, UserTypes) ->
+	istype_to_guard(Line, setelement(2, Value, Line), {type, Line, Type, TypeArgs}, UserTypes);
+do_transform({call, Line, {atom, _, asserttype}, [Value, {call, _, {atom, _, Type}, TypeArgs}]}, UserTypes) ->
+	{match, Line, 
+		{atom, Line, true},
+		istype_to_guard(Line, setelement(2, Value, Line), {type, Line, Type, TypeArgs}, UserTypes)};
 do_transform(Form, _) ->
 	Form.
 
+istype_to_guard(Line, Value, Type, UserTypes) when element(1, Value) =:= call   orelse
+                                                   element(1, Value) =:= 'case' orelse
+                                                   element(1, Value) =:= 'block' ->
+	Var = get_var(Line),
+	Steps = [{match, Line, Var, Value},
+	         istype_to_guard(Line, Var, Type, UserTypes)],
+	{block, Line, Steps};
+
 %% Literals
-istype_to_gaurd(Line, Var, {Type, _, Value}, _) ->
-	{op, Line, '=:=', {var, Line, Var}, {Type, Line, Value}};
+istype_to_guard(Line, Value, {_, _, _} = Literal, _) ->
+	{op, Line, '=:=', Value, setelement(2, Literal, Line)};
 
 %% Simple types
-istype_to_gaurd(Line, Var, {type, _, atom, _}, _) ->
-	{call, Line, {atom, Line, is_atom}, [{var, Line, Var}]};
-istype_to_gaurd(Line, Var, {type, _, binary, _}, _) ->
-	{call, Line, {atom, Line, is_binary}, [{var, Line, Var}]};
-istype_to_gaurd(Line, Var, {type, _, bitstring, _}, _) ->
-	{call, Line, {atom, Line, is_bitstring}, [{var, Line, Var}]};
-istype_to_gaurd(Line, Var, {type, _, boolean, _}, _) ->
-	{call, Line, {atom, Line, is_boolean}, [{var, Line, Var}]};
-istype_to_gaurd(Line, Var, {type, _, float, _}, _) ->
-	{call, Line, {atom, Line, is_float}, [{var, Line, Var}]};
-istype_to_gaurd(Line, Var, {type, _, function, _}, _) ->
-	{call, Line, {atom, Line, is_function}, [{var, Line, Var}]};
-istype_to_gaurd(Line, Var, {type, _, integer, _}, _) ->
-	{call, Line, {atom, Line, is_integer}, [{var, Line, Var}]};
-istype_to_gaurd(Line, Var, {type, _, list, _}, _) ->
-	{call, Line, {atom, Line, is_list}, [{var, Line, Var}]};
-istype_to_gaurd(Line, Var, {type, _, map, _}, _) ->
-	{call, Line, {atom, Line, is_map}, [{var, Line, Var}]};
-istype_to_gaurd(Line, Var, {type, _, number, _}, _) ->
-	{call, Line, {atom, Line, is_number}, [{var, Line, Var}]};
-istype_to_gaurd(Line, Var, {type, _, pid, _}, _) ->
-	{call, Line, {atom, Line, is_pid}, [{var, Line, Var}]};
-istype_to_gaurd(Line, Var, {type, _, port, _}, _) ->
-	{call, Line, {atom, Line, is_port}, [{var, Line, Var}]};
-istype_to_gaurd(Line, Var, {type, _, record, [{atom, _, RecordType}]}, _) ->
-	{call, Line, {atom, Line, is_record}, [{var, Line, Var}, {atom, Line, RecordType}]};
-istype_to_gaurd(Line, Var, {type, _, reference, _}, _) ->
-	{call, Line, {atom, Line, is_reference}, [{var, Line, Var}]};
-istype_to_gaurd(Line, Var, {type, _, tuple, _}, _) ->
-	{call, Line, {atom, Line, is_tuple}, [{var, Line, Var}]};
+istype_to_guard(Line, Value, {type, _, atom, _}, _) ->
+	istype_to_existing_guard(Line, Value, is_atom, []);
+istype_to_guard(Line, Value, {type, _, binary, _}, _) ->
+	istype_to_existing_guard(Line, Value, is_binary, []);
+istype_to_guard(Line, Value, {type, _, bitstring, _}, _) ->
+	istype_to_existing_guard(Line, Value, is_bitstring, []);
+istype_to_guard(Line, Value, {type, _, boolean, _}, _) ->
+	istype_to_existing_guard(Line, Value, is_boolean, []);
+istype_to_guard(Line, Value, {type, _, float, _}, _) ->
+	istype_to_existing_guard(Line, Value, is_float, []);
+istype_to_guard(Line, Value, {type, _, function, _}, _) ->
+	istype_to_existing_guard(Line, Value, is_function, []);
+istype_to_guard(Line, Value, {type, _, integer, _}, _) ->
+	istype_to_existing_guard(Line, Value, is_integer, []);
+istype_to_guard(Line, Value, {type, _, list, _}, _) ->
+	istype_to_existing_guard(Line, Value, is_list, []);
+istype_to_guard(Line, Value, {type, _, map, _}, _) ->
+	istype_to_existing_guard(Line, Value, is_map, []);
+istype_to_guard(Line, Value, {type, _, number, _}, _) ->
+	istype_to_existing_guard(Line, Value, is_number, []);
+istype_to_guard(Line, Value, {type, _, pid, _}, _) ->
+	istype_to_existing_guard(Line, Value, is_pid, []);
+istype_to_guard(Line, Value, {type, _, port, _}, _) ->
+	istype_to_existing_guard(Line, Value, is_port, []);
+istype_to_guard(Line, Value, {type, _, record, [{atom, _, RecordType}]}, _) ->
+	istype_to_existing_guard(Line, Value, is_record, [{atom, Line, RecordType}]);
+istype_to_guard(Line, Value, {type, _, reference, _}, _) ->
+	istype_to_existing_guard(Line, Value, is_reference, []);
+istype_to_guard(Line, Value, {type, _, tuple, _}, _) ->
+	istype_to_existing_guard(Line, Value, is_tuple, []);
 
 %% Compound types
-istype_to_gaurd(Line, Var, {type, _, range, [{LowType, _, Low}, {HighType, _, High}]}, _) ->
+istype_to_guard(Line, Value, {type, _, range, [{LowType, _, _} = Low, High]}, _) ->
 	{op, Line, 'andalso',
-		{call, Line, {atom, Line, is_integer}, [{var, Line, Var}]},
-		{op, Line, 'orelse',
-			{op, Line, '>=', {var, Line, Var}, {LowType, Line, Low}},
-			{op, Line, '=<', {var, Line, Var}, {HighType, Line, High}}}};
+		istype_to_guard(Line, Value, {type, Line, LowType, []}, []),
+		{op, Line, 'andalso',
+			{op, Line, '>=', Value, setelement(2, Low, Line)},
+			{op, Line, '=<', Value, setelement(2, High, Line)}}};
 
-istype_to_gaurd(Line, Var, {type, _, union, [Type1, Type2]}, UserTypes) ->
+istype_to_guard(Line, Value, {type, _, union, [Type1, Type2]}, UserTypes) ->
 	{op, Line, 'orelse',
-		istype_to_gaurd(Line, Var, Type1, UserTypes),
-		istype_to_gaurd(Line, Var, Type2, UserTypes)};
-istype_to_gaurd(Line, Var, {type, _, union, [Type | Types]}, UserTypes) ->
+		istype_to_guard(Line, Value, Type1, UserTypes),
+		istype_to_guard(Line, Value, Type2, UserTypes)};
+istype_to_guard(Line, Value, {type, _, union, [Type | Types]}, UserTypes) ->
 	{op, Line, 'orelse',
-		istype_to_gaurd(Line, Var, Type, UserTypes),
-		istype_to_gaurd(Line, Var, {type, Line, union, Types}, UserTypes)};
+		istype_to_guard(Line, Value, Type, UserTypes),
+		istype_to_guard(Line, Value, {type, Line, union, Types}, UserTypes)};
 	
-istype_to_gaurd(Line, Var, {_, _, Type, _}, UserTypes) ->
-	istype_to_gaurd(Line, Var, maps:get(Type, UserTypes), UserTypes).
+istype_to_guard(Line, Value, {_, _, Type, _}, UserTypes) ->
+	istype_to_guard(Line, Value, maps:get(Type, UserTypes), UserTypes).
+
+istype_to_existing_guard(Line, Value, Guard, Args) ->
+	{call, Line, {atom, Line, Guard}, [Value | Args]}.
+
+get_var(Line) ->
+	Var = case get('__var_counter__') of
+	          undefined -> 1;
+	          Counter -> Counter + 1
+	      end,
+	put('__var_counter__', Var),
+	{var, Line, list_to_atom("__IsType_" ++ integer_to_list(Var))}.
+
+
