71675e32bdc3054c596f916c9acd5a0dde9f1982
Charles Zilm <ch@rleszilm.com>
2017-08-18 21:47:36 -0700

    Change indentation

--- src/istype.erl
+++ src/istype.erl
@@ -6,12 +6,12 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -record(record0, {a = apple  :: atom(),
-	              b = <<"">> :: binary(),
-	              c          :: undefined | range(),
-	              d}).
+                  b = <<"">> :: binary(),
+                  c          :: undefined | range(),
+                  d}).
 
 -record(record1, {a = #record0{} :: record0(),
-	              b = <<"">> :: binary()}).
+                  b = <<"">> :: binary()}).
 
 -type custom()          :: binary() | integer() | atom.
 -type compound_custom() :: custom() | float().
@@ -25,146 +25,146 @@
 -type record1()         :: #record1{}.
 
 -export_type([custom/0,
-	          compound_custom/0,
-	          range/0,
-	          tuple0/0,
-	          tuple1/0,
-	          tuple2/0,
-	          tuple3/0,
-	          tuple_union/0,
-	          record0/0,
-	          record1/0]).
+              compound_custom/0,
+              range/0,
+              tuple0/0,
+              tuple1/0,
+              tuple2/0,
+              tuple3/0,
+              tuple_union/0,
+              record0/0,
+              record1/0]).
 
 -define(validate(Expected, Type), lists:foreach(fun(Value) ->
-	                                                io:format("~p = istype(~p, ~p())\n", [Expected, Value, Type]),
-	                                                Expected = istype(Value, Type())
-	                                            end,
-	                                            Type(Expected))).
+                                                    io:format("~p = istype(~p, ~p())\n", [Expected, Value, Type]),
+                                                    Expected = istype(Value, Type())
+                                                end,
+                                                Type(Expected))).
 
 %%====================================================================
 %% Test functions
 %%====================================================================
 assert_test() ->
-	true = asserttype(atom, atom()),
-	true = try
-	           asserttype(<<"binary">>, atom()),
-	           false
-	       catch
-	           error:{badmatch, false} ->
-	               true
-	       end.
+    true = asserttype(atom, atom()),
+    true = try
+               asserttype(<<"binary">>, atom()),
+               false
+           catch
+               error:{badmatch, false} ->
+                   true
+           end.
 
 invocation_test() ->
-	TestAtom = atom,
-	TestRecord = #record0{},
-	true = istype(TestRecord#record0.a, atom()),
-	true = istype(atom, atom()),
-	true = istype(TestAtom, atom()),
-	true = istype(fun() -> atom end(), atom()),
-	true = istype(return_atom(), atom()),
-	true = istype(?MODULE:return_atom(), atom()),
-	true = istype(case atom of X0 -> X0 end, atom()),
-	true = istype(begin return_atom() end, atom()),
-	true = case ?MODULE:return_atom() of
-	           X1 when istype(X1, atom()) -> true
-	       end,
-	true = fun(X2) when istype(X2, atom()) -> true;
-		      (_) -> false
-		   end(TestAtom).
+    TestAtom = atom,
+    TestRecord = #record0{},
+    true = istype(TestRecord#record0.a, atom()),
+    true = istype(atom, atom()),
+    true = istype(TestAtom, atom()),
+    true = istype(fun() -> atom end(), atom()),
+    true = istype(return_atom(), atom()),
+    true = istype(?MODULE:return_atom(), atom()),
+    true = istype(case atom of X0 -> X0 end, atom()),
+    true = istype(begin return_atom() end, atom()),
+    true = case ?MODULE:return_atom() of
+               X1 when istype(X1, atom()) -> true
+           end,
+    true = fun(X2) when istype(X2, atom()) -> true;
+              (_) -> false
+           end(TestAtom).
 
 custom_type_test() ->
-	true  = istype(<<"hello">>, custom()),
-	true  = istype(1,           custom()),
-	true  = istype(atom,        custom()),
-	false = istype([],          custom()),
-	true  = istype(7.0,         compound_custom()),
-	true  = case 7.0 of
-	            X when istype(X, compound_custom()) -> true;
-	            _ -> false
-	        end.
+    true  = istype(<<"hello">>, custom()),
+    true  = istype(1,           custom()),
+    true  = istype(atom,        custom()),
+    false = istype([],          custom()),
+    true  = istype(7.0,         compound_custom()),
+    true  = case 7.0 of
+                X when istype(X, compound_custom()) -> true;
+                _ -> false
+            end.
 
 range_test() ->
-	true  = istype(1,   range()),
-	true  = istype(50,  range()),
-	true  = istype(100, range()),
-	false = istype(0,   range()),
-	false = istype(101, range()),
-	true  = case 10 of
-	            X when istype(X, range()) -> true;
-	            _ -> false
-	        end.
+    true  = istype(1,   range()),
+    true  = istype(50,  range()),
+    true  = istype(100, range()),
+    false = istype(0,   range()),
+    false = istype(101, range()),
+    true  = case 10 of
+                X when istype(X, range()) -> true;
+                _ -> false
+            end.
 
 record_test() ->
-	?validate(true, record0),
-	?validate(false, record0),
+    ?validate(true, record0),
+    ?validate(false, record0),
 
-	?validate(true, record1),
-	?validate(false, record1).
+    ?validate(true, record1),
+    ?validate(false, record1).
 
 tuple_test() ->
-	?validate(true,  tuple0),
-	?validate(false, tuple0),
+    ?validate(true,  tuple0),
+    ?validate(false, tuple0),
 
-	?validate(true,  tuple1),
-	?validate(false, tuple1),
+    ?validate(true,  tuple1),
+    ?validate(false, tuple1),
 
-	?validate(true,  tuple2),
-	?validate(false, tuple2),
+    ?validate(true,  tuple2),
+    ?validate(false, tuple2),
 
-	?validate(true,  tuple3),
-	?validate(false, tuple3),
+    ?validate(true,  tuple3),
+    ?validate(false, tuple3),
 
-	?validate(true,  tuple_union),
-	?validate(false, tuple_union),
+    ?validate(true,  tuple_union),
+    ?validate(false, tuple_union),
 
-	true  = case hd(tuple3(true)) of
-		        X when istype(X, tuple3()) -> true;
-		        _ -> false
-	        end. 
+    true  = case hd(tuple3(true)) of
+                X when istype(X, tuple3()) -> true;
+                _ -> false
+            end. 
 
 
 %%====================================================================
 %% Utility functions
 %%====================================================================
 return_atom() ->
-	atom.
+    atom.
 
 record0(true) ->
-	[#record0{},
-	 #record0{a = also,
-	          b = <<"also">>,
-	          c = 50}];
+    [#record0{},
+     #record0{a = also,
+              b = <<"also">>,
+              c = 50}];
 record0(false) ->
-	[<<"">>, {}, #record0{c = atom}, {record0}, #record1{}].
+    [<<"">>, {}, #record0{c = atom}, {record0}, #record1{}].
 
 record1(true) ->
-	[#record1{},
-	 #record1{b = <<"also">>}];
+    [#record1{},
+     #record1{b = <<"also">>}];
 record1(false) ->
-	[<<"">>, {}, #record0{}, {record0}, #record1{a = undefined}].
+    [<<"">>, {}, #record0{}, {record0}, #record1{a = undefined}].
 
 tuple0(true) -> 
-	[{}];
+    [{}];
 tuple0(false) ->
-	[<<"">>, {1}].
+    [<<"">>, {1}].
 
 tuple1(true) ->
-	[{atom}];
+    [{atom}];
 tuple1(false) ->
-	[<<"">>, {1}, {1, 2}].
+    [<<"">>, {1}, {1, 2}].
 
 tuple2(true) ->
-	[{atom, atom}, {atom, also}];
+    [{atom, atom}, {atom, also}];
 tuple2(false) ->
-	[<<"">>, {atom}, {also, also}, {atom, <<"binary">>}, {atom, atom, <<"binary">>}].
+    [<<"">>, {atom}, {also, also}, {atom, <<"binary">>}, {atom, atom, <<"binary">>}].
 
 tuple3(true) ->
-	[{atom, atom, <<"binary">>}, {atom, also, <<"also">>}];
+    [{atom, atom, <<"binary">>}, {atom, also, <<"also">>}];
 tuple3(false) ->
-	[<<"">>, {atom, <<"binary">>}, {also, atom, <<"binary">>}, {atom, atom, atom}, {atom, atom, <<"binary">>, <<"binary">>}].
+    [<<"">>, {atom, <<"binary">>}, {also, atom, <<"binary">>}, {atom, atom, atom}, {atom, atom, <<"binary">>, <<"binary">>}].
 
 tuple_union(true) ->
-	[{atom}, {atom, atom, <<"binary">>}, {atom, also, <<"also">>}];
+    [{atom}, {atom, atom, <<"binary">>}, {atom, also, <<"also">>}];
 tuple_union(false) ->
-	[<<"">>, {}, {<<"">>}, {atom, atom}, {also, also, also}, {atom, atom, atom, atom}].
+    [<<"">>, {}, {<<"">>}, {atom, atom}, {also, also, also}, {atom, atom, atom, atom}].
 
--- src/istype_transform.erl
+++ src/istype_transform.erl
@@ -4,161 +4,161 @@
 %% @doc
 %% @end
 parse_transform(Forms, _Options) ->
-	UserTypes = forms:reduce(fun get_types/2, #{types => #{}, records => #{}}, Forms),
-	forms:map(fun(Form) -> do_transform(Form, UserTypes) end, Forms).
+    UserTypes = forms:reduce(fun get_types/2, #{types => #{}, records => #{}}, Forms),
+    forms:map(fun(Form) -> do_transform(Form, UserTypes) end, Forms).
 
 get_types({attribute, _, type, {Type, TypeSpec, []}}, #{types := Acc1} = Acc0) ->
-	Acc0#{types => Acc1#{Type => TypeSpec}};
+    Acc0#{types => Acc1#{Type => TypeSpec}};
 get_types({attribute, _, record, {Record, RecordSpec}}, #{records := Acc1} = Acc0) ->
-	Acc0#{records => Acc1#{Record => {length(RecordSpec) + 1,
-	                                  lists:filter(fun(RecordField) when element(1, RecordField) =:= typed_record_field -> true;
-		                                              (_) -> false
-		                                           end,
-		                                           RecordSpec)}}};
+    Acc0#{records => Acc1#{Record => {length(RecordSpec) + 1,
+                                      lists:filter(fun(RecordField) when element(1, RecordField) =:= typed_record_field -> true;
+                                                      (_) -> false
+                                                   end,
+                                                   RecordSpec)}}};
 get_types(_, Acc) -> Acc.
 
 do_transform({call, Line, {atom, _, istype}, [Value, {call, _, {atom, _, Type}, TypeArgs}]}, UserTypes) ->
-	optimal_istype_to_guard(Line, setelement(2, Value, Line), {type, Line, Type, TypeArgs}, UserTypes);
+    optimal_istype_to_guard(Line, setelement(2, Value, Line), {type, Line, Type, TypeArgs}, UserTypes);
 do_transform({call, Line, {atom, _, asserttype}, Args}, UserTypes) ->
-	{match, Line, 
-		{atom, Line, true},
-		do_transform({call, Line, {atom, Line, istype}, Args}, UserTypes)};
+    {match, Line, 
+        {atom, Line, true},
+        do_transform({call, Line, {atom, Line, istype}, Args}, UserTypes)};
 do_transform(Form, _) ->
-	Form.
+    Form.
 
 optimal_istype_to_guard(Line, {call, _, {atom, _, element} = Value, Args}, Type, UserTypes) when length(Args) =:= 2 ->
-	istype_to_guard(Line, Value, Type, UserTypes);
+    istype_to_guard(Line, Value, Type, UserTypes);
 optimal_istype_to_guard(Line, Value, Type, UserTypes) when element(1, Value) =:= call   orelse
                                                            element(1, Value) =:= 'case' orelse
                                                            element(1, Value) =:= 'block' ->
-	Var = get_var(Line),
-	Steps = [{match, Line, Var, Value},
-	         istype_to_guard(Line, Var, Type, UserTypes)],
-	{block, Line, Steps};
+    Var = get_var(Line),
+    Steps = [{match, Line, Var, Value},
+             istype_to_guard(Line, Var, Type, UserTypes)],
+    {block, Line, Steps};
 optimal_istype_to_guard(Line, Var, Type, UserTypes) ->
-	istype_to_guard(Line, Var, Type, UserTypes).
+    istype_to_guard(Line, Var, Type, UserTypes).
 
 %% Literals
 istype_to_guard(Line, Value, {_, _, _} = Literal, _) ->
-	{op, Line, '=:=', Value, setelement(2, Literal, Line)};
+    {op, Line, '=:=', Value, setelement(2, Literal, Line)};
 
 %% Simple types
 istype_to_guard(Line, Value, {type, _, atom, _}, _) ->
-	istype_to_existing_guard(Line, Value, is_atom, []);
+    istype_to_existing_guard(Line, Value, is_atom, []);
 istype_to_guard(Line, Value, {type, _, binary, _}, _) ->
-	istype_to_existing_guard(Line, Value, is_binary, []);
+    istype_to_existing_guard(Line, Value, is_binary, []);
 istype_to_guard(Line, Value, {type, _, bitstring, _}, _) ->
-	istype_to_existing_guard(Line, Value, is_bitstring, []);
+    istype_to_existing_guard(Line, Value, is_bitstring, []);
 istype_to_guard(Line, Value, {type, _, boolean, _}, _) ->
-	istype_to_existing_guard(Line, Value, is_boolean, []);
+    istype_to_existing_guard(Line, Value, is_boolean, []);
 istype_to_guard(Line, Value, {type, _, float, _}, _) ->
-	istype_to_existing_guard(Line, Value, is_float, []);
+    istype_to_existing_guard(Line, Value, is_float, []);
 istype_to_guard(Line, Value, {type, _, function, _}, _) ->
-	istype_to_existing_guard(Line, Value, is_function, []);
+    istype_to_existing_guard(Line, Value, is_function, []);
 istype_to_guard(Line, Value, {type, _, integer, _}, _) ->
-	istype_to_existing_guard(Line, Value, is_integer, []);
+    istype_to_existing_guard(Line, Value, is_integer, []);
 istype_to_guard(Line, Value, {type, _, list, _}, _) ->
-	istype_to_existing_guard(Line, Value, is_list, []);
+    istype_to_existing_guard(Line, Value, is_list, []);
 istype_to_guard(Line, Value, {type, _, map, _}, _) ->
-	istype_to_existing_guard(Line, Value, is_map, []);
+    istype_to_existing_guard(Line, Value, is_map, []);
 istype_to_guard(Line, Value, {type, _, number, _}, _) ->
-	istype_to_existing_guard(Line, Value, is_number, []);
+    istype_to_existing_guard(Line, Value, is_number, []);
 istype_to_guard(Line, Value, {type, _, pid, _}, _) ->
-	istype_to_existing_guard(Line, Value, is_pid, []);
+    istype_to_existing_guard(Line, Value, is_pid, []);
 istype_to_guard(Line, Value, {type, _, port, _}, _) ->
-	istype_to_existing_guard(Line, Value, is_port, []);
+    istype_to_existing_guard(Line, Value, is_port, []);
 istype_to_guard(Line, Value, {type, _, reference, _}, _) ->
-	istype_to_existing_guard(Line, Value, is_reference, []);
+    istype_to_existing_guard(Line, Value, is_reference, []);
 
 %% Compound types
 istype_to_guard(Line, Value, {type, _, range, [{LowType, _, _} = Low, High]}, _) ->
-	{op, Line, 'andalso',
-		istype_to_guard(Line, Value, {type, Line, LowType, []}, []),
-		{op, Line, 'andalso',
-			{op, Line, '>=', Value, setelement(2, Low, Line)},
-			{op, Line, '=<', Value, setelement(2, High, Line)}}};
+    {op, Line, 'andalso',
+        istype_to_guard(Line, Value, {type, Line, LowType, []}, []),
+        {op, Line, 'andalso',
+            {op, Line, '>=', Value, setelement(2, Low, Line)},
+            {op, Line, '=<', Value, setelement(2, High, Line)}}};
 
 istype_to_guard(Line, Value, {type, _, record, [{atom, _, RecordType}]}, UserTypes) ->
-	#{records := #{RecordType := {RecordSize, RecordSpec}}} = UserTypes,
+    #{records := #{RecordType := {RecordSize, RecordSpec}}} = UserTypes,
 
-	case RecordSize of
-		1 ->
-			istype_to_existing_guard(Line, Value, is_record, [{atom, Line, RecordType},
-					                                          {integer, Line, RecordSize}]);
-		_ -> 
-			{op, Line, 'andalso',
-				istype_to_existing_guard(Line, Value, is_record, [{atom, Line, RecordType},
-					                                              {integer, Line, RecordSize}]),
-				record_guard(Line, Value, RecordType, RecordSpec, UserTypes)}
-	end;
+    case RecordSize of
+        1 ->
+            istype_to_existing_guard(Line, Value, is_record, [{atom, Line, RecordType},
+                                                              {integer, Line, RecordSize}]);
+        _ -> 
+            {op, Line, 'andalso',
+                istype_to_existing_guard(Line, Value, is_record, [{atom, Line, RecordType},
+                                                                  {integer, Line, RecordSize}]),
+                record_guard(Line, Value, RecordType, RecordSpec, UserTypes)}
+    end;
 
 istype_to_guard(Line, Value, {type, _, tuple, Types}, UserTypes) ->
-	case length(Types) of
-		0 ->
-			{op, Line, 'andalso',
-				{call, Line, {atom, Line, 'is_tuple'}, [Value]},
-				{op, Line, '=:=',
-					{call, Line, {atom, Line, tuple_size}, [Value]},
-					{integer, Line, 0}}};
-		Length ->
-			{op, Line, 'andalso',
-				{call, Line, {atom, Line, 'is_tuple'}, [Value]},
-				{op, Line, 'andalso',
-					{op, Line, '=:=',
-						{call, Line, {atom, Line, tuple_size}, [Value]},
-						{integer, Line, Length}},
-					tuple_guard(Line, 1, Value, Types, UserTypes)}}
-	end;
+    case length(Types) of
+        0 ->
+            {op, Line, 'andalso',
+                {call, Line, {atom, Line, 'is_tuple'}, [Value]},
+                {op, Line, '=:=',
+                    {call, Line, {atom, Line, tuple_size}, [Value]},
+                    {integer, Line, 0}}};
+        Length ->
+            {op, Line, 'andalso',
+                {call, Line, {atom, Line, 'is_tuple'}, [Value]},
+                {op, Line, 'andalso',
+                    {op, Line, '=:=',
+                        {call, Line, {atom, Line, tuple_size}, [Value]},
+                        {integer, Line, Length}},
+                    tuple_guard(Line, 1, Value, Types, UserTypes)}}
+    end;
 
 istype_to_guard(Line, Value, {type, _, union, Types}, UserTypes) ->
-	union_guard(Line, Value, Types, UserTypes);
+    union_guard(Line, Value, Types, UserTypes);
 
 istype_to_guard(Line, Value, {TypeClass, _, Type, _}, UserTypes) when TypeClass =:= type orelse
                                                                       TypeClass =:= user_type ->
-	#{types := #{Type := TypeSpec}} = UserTypes,
-	istype_to_guard(Line, Value, TypeSpec, UserTypes).
+    #{types := #{Type := TypeSpec}} = UserTypes,
+    istype_to_guard(Line, Value, TypeSpec, UserTypes).
 
 istype_to_existing_guard(Line, Value, Guard, Args) ->
-	{call, Line, {atom, Line, Guard}, [Value | Args]}.
+    {call, Line, {atom, Line, Guard}, [Value | Args]}.
 
 get_var(Line) ->
-	Var = case get('__var_counter__') of
-	          undefined -> 1;
-	          Counter -> Counter + 1
-	      end,
-	put('__var_counter__', Var),
-	{var, Line, list_to_atom("__IsType_" ++ integer_to_list(Var))}.
+    Var = case get('__var_counter__') of
+              undefined -> 1;
+              Counter -> Counter + 1
+          end,
+    put('__var_counter__', Var),
+    {var, Line, list_to_atom("__IsType_" ++ integer_to_list(Var))}.
 
 record_guard(Line, Value, RecordType, [RecordFieldSpec], UserTypes) ->
-	{Field, Type} = case RecordFieldSpec of
-	                    {_, {_, _, F, _}, T} -> {F, T};
-	                    {_, {_, _, F}, T} -> {F, T}
-	                end,
-	istype_to_guard(Line, {record_field, Line, Value, RecordType, setelement(2, Field, Line)}, Type, UserTypes);
+    {Field, Type} = case RecordFieldSpec of
+                        {_, {_, _, F, _}, T} -> {F, T};
+                        {_, {_, _, F}, T} -> {F, T}
+                    end,
+    istype_to_guard(Line, {record_field, Line, Value, RecordType, setelement(2, Field, Line)}, Type, UserTypes);
 record_guard(Line, Value, RecordType, [RecordFieldSpec | RecordFieldSpecs], UserTypes) ->
-	{Field, Type} = case RecordFieldSpec of
-	                    {_, {_, _, F, _}, T} -> {F, T};
-	                    {_, {_, _, F}, T} -> {F, T}
-	                end,
-	{op, Line, 'andalso',
-		istype_to_guard(Line, {record_field, Line, Value, RecordType, setelement(2, Field, Line)}, Type, UserTypes),
-		record_guard(Line, Value, RecordType, RecordFieldSpecs, UserTypes)}.
+    {Field, Type} = case RecordFieldSpec of
+                        {_, {_, _, F, _}, T} -> {F, T};
+                        {_, {_, _, F}, T} -> {F, T}
+                    end,
+    {op, Line, 'andalso',
+        istype_to_guard(Line, {record_field, Line, Value, RecordType, setelement(2, Field, Line)}, Type, UserTypes),
+        record_guard(Line, Value, RecordType, RecordFieldSpecs, UserTypes)}.
 
 tuple_guard(Line, Index, Value0, [Type], UserTypes) ->
-	Value1 = {call, Line, {atom, Line, element}, [{integer, Line, Index}, Value0]},
-	istype_to_guard(Line, Value1, Type, UserTypes);
+    Value1 = {call, Line, {atom, Line, element}, [{integer, Line, Index}, Value0]},
+    istype_to_guard(Line, Value1, Type, UserTypes);
 
 tuple_guard(Line, Index, Value0, [Type | Types], UserTypes) ->
-	Value1 = {call, Line, {atom, Line, element}, [{integer, Line, Index}, Value0]},
-	{op, Line, 'andalso',
-		istype_to_guard(Line, Value1, Type, UserTypes),
-		tuple_guard(Line, Index+1, Value0, Types, UserTypes)}.
+    Value1 = {call, Line, {atom, Line, element}, [{integer, Line, Index}, Value0]},
+    {op, Line, 'andalso',
+        istype_to_guard(Line, Value1, Type, UserTypes),
+        tuple_guard(Line, Index+1, Value0, Types, UserTypes)}.
 
 union_guard(Line, Value, [Type1, Type2], UserTypes) ->
-	{op, Line, 'orelse',
-		istype_to_guard(Line, Value, Type1, UserTypes),
-		istype_to_guard(Line, Value, Type2, UserTypes)};
+    {op, Line, 'orelse',
+        istype_to_guard(Line, Value, Type1, UserTypes),
+        istype_to_guard(Line, Value, Type2, UserTypes)};
 union_guard(Line, Value, [Type | Types], UserTypes) ->
-	{op, Line, 'orelse',
-		istype_to_guard(Line, Value, Type, UserTypes),
-		istype_to_guard(Line, Value, {type, Line, union, Types}, UserTypes)}.
+    {op, Line, 'orelse',
+        istype_to_guard(Line, Value, Type, UserTypes),
+        istype_to_guard(Line, Value, {type, Line, union, Types}, UserTypes)}.
